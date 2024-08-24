344f287c8dd9c788377f1fefcb7d622f9530a56e
Nadir <mutikn@mail.ru>
2024-08-24 23:51:59 +0400

    Simple JavaScript code

---
+++ script.js
@@ -0,0 +1,25 @@
+let cartQuantity = 0;
+
+function showQuantity(){
+    console.log(`Cart quantity ${cartQuantity}`);
+}
+
+function addToCart(){
+    cartQuantity += 1;
+    console.log(`Cart quantity ${cartQuantity}`);
+}
+
+function plusTwo(){
+    cartQuantity += 2;
+    console.log(`Cart quantity ${cartQuantity}`);
+}
+
+function plusThree(){
+    cartQuantity += 3;
+    console.log(`Cart quantity ${cartQuantity}`);
+}
+
+function resetCart(){
+    cartQuantity = cartQuantity - cartQuantity;
+    console.log(`Cart quantity ${cartQuantity}`);
+}
